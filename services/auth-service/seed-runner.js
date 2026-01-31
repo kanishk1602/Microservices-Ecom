b3361ee9fb53181020cec194d6602eb2914108a7
kanishk1602 <aaaaa.bbbb.ccc.dd.e.dd.ccc.bbbb.aaaaa@gmail.com>
2026-01-31 16:40:21 +0530

    db seeding script add

--- services/auth-service/seed-runner.js
+++ services/auth-service/seed-runner.js
@@ -1,11 +1,15 @@
 const dbConnect = require('./config/database');
 const { seedAdmin } = require('./config/seedAdmin');
+const { seedFromFile } = require('./config/seedProducts');
+const path = require('path');
 
 async function run() {
   try {
     await dbConnect.connect();
     console.log('Connected. Seeding...');
     await seedAdmin();
+    // Seed products from defaultProducts.json
+    await seedFromFile(path.join(__dirname, 'config', 'defaultProducts.json'));
     console.log('Seeding complete.');
     process.exit(0);
   } catch (err) {
